dec973ea1cce5673e7e15a62eef5b790117e338f
Avinash Kumar <32469907+avish005@users.noreply.github.com>
2020-08-12 09:27:13 +0530

    Add files via upload

--- cart_original.php
+++ cart_original.php
@@ -30,39 +30,46 @@ require('top.php');
                                 <?php
                                     if(!empty($_SESSION['cart'])){
                                 ?>
-                                <?php
-                                if(isset($_SESSION['cart'])){
-                                    foreach($_SESSION['cart'] as $key=>$val){
-                                    $productArr=get_product($con,'','',$key);
-                                    $pname=$productArr[0]['name'];
-                                    $mrp=$productArr[0]['mrp'];
-                                    $price=$productArr[0]['price'];
-                                    $image=$productArr[0]['image'];
-                                    $qty=$val['qty'];
-                                    ?>
-                                        <div class="row">
-                                            <div class="col-lg-1 col-md-1"></div>
-                                            <div class="col-lg-4 col-md-2 col-sm-2  product_img">
-                                                <img src="<?php echo PRODUCT_IMAGE_SITE_PATH.$image?>" style="max-width: 250px;"/>
-                                            </div>
-                                            <div class="col-lg-5" id="product_details">
-                                                    <p class="product_name"><?php echo $pname?></p>
-                                                    <p>MRP : <span class="old__prize"><?php echo $mrp?> </span> </p>
-                                                    <p>Offer Price : <span class="dp"><?php echo $price?></span></p>
-                                                    <p class="mobile-view">Qty : <input type="number" id="<?php echo $key?>qty" value="<?php echo $qty?>" />
-                                                        <a class="btn" id="update" href="javascript:void(0)" onclick="manage_cart('<?php echo $key?>','update')">update</a></p>
-                                                    <p class="desktop-view">Qty : <input type="number" id="<?php echo $key?>qty" value="<?php echo $qty?>" onclick="manage_cart('<?php echo $key?>','update')" style="width: 50px;" />
-                                                    <span class="not_enough_stock"></span></p>
-                                                    <p>Product Total : <span class="product-subtotal"><?php echo $qty*$price?></span></p>
-												
-                                                    <p class="btn_links">
-                                                        <a class="fr__btn product-remove" href="javascript:void(0)" onclick="manage_cart('<?php echo $key?>','remove')"> <i class="icon-trash icons"></i> Remove</a>
-                                                    </p>
-                                            </div>
-                                            <div class="col-lg-2"></div>
-
-                                        </div><hr>
-                                    <?php } } ?>
+                                <table>
+                                    <thead>
+                                        <tr>
+                                            <th class="product-thumbnail">products</th>
+                                            <th class="product-name">name of products</th>
+                                            <th class="product-price">Price</th>
+                                            <th class="product-quantity">Quantity</th>
+                                            <th class="product-subtotal">Total</th>
+                                            <th class="product-remove">Remove</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+										<?php
+										if(isset($_SESSION['cart'])){
+											foreach($_SESSION['cart'] as $key=>$val){
+											$productArr=get_product($con,'','',$key);
+											$pname=$productArr[0]['name'];
+											$mrp=$productArr[0]['mrp'];
+											$price=$productArr[0]['price'];
+											$image=$productArr[0]['image'];
+											$qty=$val['qty'];
+											?>
+											<tr>
+												<td class="product-thumbnail"><img src="<?php echo PRODUCT_IMAGE_SITE_PATH.$image?>"  /></a></td>
+												<td class="product-name"><a href="#"><?php echo $pname?></a>
+													<ul  class="pro__prize">
+														<li class="old__prize"><?php echo $mrp?></li>
+														<li><?php echo $price?></li>
+													</ul>
+												</td>
+												<td class="product-price"><span class="amount"><?php echo $price?></span></td>
+												<td class="product-quantity"><input type="number" id="<?php echo $key?>qty" value="<?php echo $qty?>" onclick="manage_cart('<?php echo $key?>','update')" />
+												<span class="not_enough_stock"></span>
+												</td>
+												<td class="product-subtotal"><?php echo $qty*$price?></td>
+												<td class="product-remove" ><a href="javascript:void(0)" onclick="manage_cart('<?php echo $key?>','remove')"><i class="icon-trash icons"></i></a></td>
+											</tr>
+											<?php } } ?>
+                                    </tbody>
+                                </table>
                             </div>
                             <div class="row">
                                 <div class="col-md-12 col-sm-12 col-xs-12">
