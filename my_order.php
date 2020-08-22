c940b084d3e4b51e762254e7f023b4dd3bd93479
Avinash Kumar <32469907+avish005@users.noreply.github.com>
2020-08-22 11:00:03 +0530

    ok

--- my_order.php
+++ my_order.php
@@ -7,8 +7,12 @@ if(!isset($_SESSION['USER_LOGIN'])){
 	</script>
 	<?php
 }
+$uid=$_SESSION['USER_ID'];
+
+$res=mysqli_query($con,"select product.name,product.image,product.price,product.mrp,product.id as pid,wishlist.id from product,wishlist where wishlist.product_id=product.id and wishlist.user_id='$uid'");
 ?>
-<div class="ht__bradcaump__area" style="background: rgba(0, 0, 0, 0) url(images/bg/4.jpg) no-repeat scroll center center / cover ;">
+
+ <div class="ht__bradcaump__area" style="background: rgba(0, 0, 0, 0) url(images/bg/4.jpg) no-repeat scroll center center / cover ;">
             <div class="ht__bradcaump__wrap">
                 <div class="container">
                     <div class="row">
@@ -27,57 +31,29 @@ if(!isset($_SESSION['USER_LOGIN'])){
         </div>
         <!-- End Bradcaump area -->
         <!-- cart-main-area start -->
-        <div class="wishlist-area ptb--100 bg__white">
+        <div class="cart-main-area ptb--100 bg__white">
             <div class="container">
                 <div class="row">
-                    <div class="col-md-12 col-sm-12 col-xs-12">
-                        <div class="wishlist-content">
-                            <form action="#">
-                                <div class="wishlist-table table-responsive">
-                                    <table>
-                                        <thead>
-                                            <tr>
-                                                <th class="product-thumbnail">Order ID</th>
-                                                <th class="product-name"><span class="nobr">Order Date</span></th>
-                                                <th class="product-price"><span class="nobr"> Address </span></th>
-                                                <th class="product-stock-stauts"><span class="nobr"> Payment Type </span></th>
-												<th class="product-stock-stauts"><span class="nobr"> Payment Status </span></th>
-												<th class="product-stock-stauts"><span class="nobr"> Order Status </span></th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-											<?php
-											$uid=$_SESSION['USER_ID'];
-											$res=mysqli_query($con,"select `order`.*,order_status.name as order_status_str from `order`,order_status where `order`.user_id='$uid' and order_status.id=`order`.order_status");
-											while($row=mysqli_fetch_assoc($res)){
-											?>
-                                            <tr>
-												<td class="product-add-to-cart"><a href="my_order_details.php?id=<?php echo $row['id']?>"> <?php echo $row['id']?></a>
-												<br/>
-												<a  href="order_pdf.php?id=<?php echo $row['id']?>" id="pdf_invoice" style=""> <i class="fa fa-file-pdf-o" aria-hidden="true"></i> <span>Invoice</span></a>
-												</td>
-                                                <td class="product-name"><?php echo $row['added_on']?></td>
-                                                <td class="product-name">
-												<?php echo $row['address']?><br/>
-												<?php echo $row['city']?><br/>
-												<?php echo $row['pincode']?>
-												</td>
-												<td class="product-name"><?php echo $row['payment_type']?></td>
-												<td class="product-name"><?php echo ucfirst($row['payment_status'])?></td>
-												<td class="product-name"><?php echo $row['order_status_str']?></td>
-                                                
-                                            </tr>
-                                            <?php } ?>
-                                        </tbody>
-                                        
-                                    </table>
-                                </div>  
-                            </form>
-                        </div>
-                    </div>
-                </div>
+                    <h2>My Order<small>(s)</small></h2><br><hr>
+                    <?php
+                        $uid=$_SESSION['USER_ID'];
+                        $res=mysqli_query($con,"select `order`.*,order_status.name as order_status_str from `order`,order_status where `order`.user_id='$uid' and order_status.id=`order`.order_status order by `order`.added_on desc");
+                        while($row=mysqli_fetch_assoc($res)){
+                           
+                    ?>
+                   
+                    <div class="col-lg-3 col-md-3 col-sm-2 product_img"  style="padding:20px; margin-bottom:20px;">
+                        <p>ORDER ID<a href="my_order_details.php?id=<?php echo $row['id']?>" class="btn btn--blue btn-block"> <?php echo $row['id']?></a></p><br>
+                        <h3><a href="order_pdf.php?id=<?php echo $row['id']?>" id="pdf_invoice" style=""> <i class="fa fa-file-pdf-o" aria-hidden="true"></i> <span>Invoice</span></a></h3><br>
+                        <h3>Purchased on - <span><?php echo $row['added_on']?></span></h3><br>
+                        <h3>Order Status - <span><?php echo $row['order_status_str']?></span></h3>
+                    </div>    
+                    <div class="col-lg-1"></div>
+                    <?php } ?>
+                </div>      
             </div>
         </div>
         
-        						
+        <input type="hidden" id="qty" value="1"/>	
+        
 <?php require('footer.php')?>        
--- my_order_details.php
+++ my_order_details.php
@@ -9,16 +9,16 @@ if(!isset($_SESSION['USER_LOGIN'])){
 }
 $order_id=get_safe_value($con,$_GET['id']);
 ?>
-<div class="ht__bradcaump__area" style="background: rgba(0, 0, 0, 0) url(images/bg/4.jpg) no-repeat scroll center center / cover ;">
+ <div class="ht__bradcaump__area" style="background: rgba(0, 0, 0, 0) url(images/bg/4.jpg) no-repeat scroll center center / cover ;">
             <div class="ht__bradcaump__wrap">
                 <div class="container">
                     <div class="row">
                         <div class="col-xs-12">
                             <div class="bradcaump__inner">
                                 <nav class="bradcaump-inner">
-                                  <a class="breadcrumb-item" href="index.html">Home</a>
+                                  <a class="breadcrumb-item" href="index.php">Home</a>
                                   <span class="brd-separetor"><i class="zmdi zmdi-chevron-right"></i></span>
-                                  <span class="breadcrumb-item active">Thank You</span>
+                                  <span class="breadcrumb-item active">Order Detail</span>
                                 </nav>
                             </div>
                         </div>
@@ -28,56 +28,37 @@ $order_id=get_safe_value($con,$_GET['id']);
         </div>
         <!-- End Bradcaump area -->
         <!-- cart-main-area start -->
-        <div class="wishlist-area ptb--100 bg__white">
+        <div class="cart-main-area ptb--100 bg__white">
             <div class="container">
-                <div class="row">
-                    <div class="col-md-12 col-sm-12 col-xs-12">
-                        <div class="wishlist-content">
-                            <form action="#">
-                                <div class="wishlist-table table-responsive">
-                                    <table>
-                                        <thead>
-                                            <tr>
-                                                <th class="product-thumbnail">Product Name</th>
-												<th class="product-thumbnail">Product Image</th>
-                                                <th class="product-name">Qty</th>
-                                                <th class="product-price">Price</th>
-                                                <th class="product-price">Total Price</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-											<?php
-											$uid=$_SESSION['USER_ID'];
-											$res=mysqli_query($con,"select distinct(order_detail.id) ,order_detail.*,product.name,product.image from order_detail,product ,`order` where order_detail.order_id='$order_id' and `order`.user_id='$uid' and order_detail.product_id=product.id");
-											$total_price=0;
-											while($row=mysqli_fetch_assoc($res)){
-											$total_price=$total_price+($row['qty']*$row['price']);
-											?>
-                                            <tr>
-												<td class="product-name"><?php echo $row['name']?></td>
-                                                <td class="product-name"> <img src="<?php echo PRODUCT_IMAGE_SITE_PATH.$row['image']?>"></td>
-												<td class="product-name"><?php echo $row['qty']?></td>
-												<td class="product-name"><?php echo $row['price']?></td>
-												<td class="product-name"><?php echo $row['qty']*$row['price']?></td>
-                                                
-                                            </tr>
-                                            <?php } ?>
-											<tr>
-												<td colspan="3"></td>
-												<td class="product-name">Total Price</td>
-												<td class="product-name"><?php echo $total_price?></td>
-                                                
-                                            </tr>
-                                        </tbody>
-                                        
-                                    </table>
-                                </div>  
-                            </form>
-                        </div>
-                    </div>
-                </div>
+                <?php
+                    $uid=$_SESSION['USER_ID'];
+                    $res=mysqli_query($con,"select distinct(order_detail.id) ,order_detail.*,product.name,product.image, `order`.* from order_detail,product ,`order` where  `order`.id = '$order_id' and order_detail.order_id='$order_id' and `order`.user_id='$uid' and order_detail.product_id=product.id");
+                    $total_price=0;
+                    while($row=mysqli_fetch_assoc($res)){
+                    $total_price=$total_price+($row['qty']*$row['price']);
+                ?>
+                    <div class="row">
+                   <div class="col-lg-3 col-md-2 col-sm-2 product_img">
+                        <img src="<?php echo PRODUCT_IMAGE_SITE_PATH.$row['image']?>" style="max-width: 200px;"/>
+                   </div>
+                   <div class="col-lg-5" id="product_details">
+                        <p class="product_name"><?php echo $row['name']?></p>
+                        <p class="qty">Qty : <?php echo $row['qty']?></p>
+                        <p>Price : <span class="dp"><?php echo $row['price']?></span></p>
+                        <p>Total Price : <span class="dp"><?php echo $total_price?></span></p>
+
+                        <br>
+
+                        <p class="product_name"><strong>Address :</strong><br> <?php echo $row['address']?></p>
+                        <p class="qty">City:  <?php echo $row['city']?></p>
+                        <p>Pincode : <?php echo $row['pincode']?></span></p>
+                        
+                   </div>
+                    <?php } ?>
+                </div>      
             </div>
         </div>
         
-        						
+        <input type="hidden" id="qty" value="1"/>	
+        
 <?php require('footer.php')?>        
